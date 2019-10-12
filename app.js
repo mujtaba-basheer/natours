c2015082b121980f8d4da50111627dbc5630b319
mujtaba-basheer <mujtababasheer14@gmail.com>
2019-10-13 01:06:59 +0530

    Added Database error handling

--- app.js
+++ app.js
@@ -69,18 +69,6 @@ app.all('*', (req, res, next) => {
 //   .get(getAllTours)
 //   .post(createTour);
 
-app.use((err, req, res, next) => {
-  console.log(err.stack);
-
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
-
 //original API without refactoring route
 
 // app.delete('/api/v1/tours/:id', (req,res) => {
