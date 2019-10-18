4865b981118544d9f792fa8ef06330489ea90d8d
mujtaba-basheer <mujtababasheer14@gmail.com>
2019-10-18 21:30:22 +0530

    Set up SignUp and Login functionality

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  onsole.log(err.name, err.message);
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
